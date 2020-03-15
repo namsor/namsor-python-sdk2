e29d067cf5f4045fdc372f1473b61bf111087dad
NamSor SAS <contact@namsor.com>
2020-03-15 12:55:47 +0100

    Minor update

--- run-python.bash
+++ run-python.bash
@@ -2,7 +2,7 @@ echo update openapi-generator
 ./bin/utils/openapi-generator-cli.sh
 rm -Rf ./namsor/client/python/*
 echo run openapi-generator
-java -jar modules/openapi-generator-cli/target/openapi-generator-cli.jar generate  --git-repo-id namsor-python-sdk2 --git-user-id namsor --skip-validate-spec -i https://v2.namsor.com/NamSorAPIv2/api2/openapi.json -g python -o  namsor/client/python  --additional-properties packageVersion=2.0.8
+java -jar modules/openapi-generator-cli/target/openapi-generator-cli.jar generate  --git-repo-id namsor-python-sdk2 --git-user-id namsor --skip-validate-spec -i https://v2.namsor.com/NamSorAPIv2/api2/openapi.json -g python -o  namsor/client/python  --additional-properties packageVersion=2.0.9
 #~/dropbox_uploader.sh upload namsor-sdk2-1.0.0.jar namsor-sdk2-1.0.0.jar
 #~/dropbox_uploader.sh upload namsor-sdk2-1.0.0-javadoc.jar namsor-sdk2-1.0.0-javadoc.jar
 #~/dropbox_uploader.sh upload namsor-sdk2-1.0.0-sources.jar namsor-sdk2-1.0.0-sources.jar
